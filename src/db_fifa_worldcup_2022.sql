b02e33b429b5f4f1ae5437574aa6a445838130bc
Davidteje <davidtejesan@gmail.com>
2022-12-11 13:45:15 +0100

    commit

--- src/db_fifa_worldcup_2022.sql
+++ src/db_fifa_worldcup_2022.sql
@@ -18,41 +18,6 @@ CREATE SCHEMA IF NOT EXISTS `fifa_worldcup_2022` DEFAULT CHARACTER SET utf8mb4 C
 USE `fifa_worldcup_2022` ;
 
 -- -----------------------------------------------------
--- Table `fifa_worldcup_2022`.`teams`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `fifa_worldcup_2022`.`teams` (
-  `team_id` BIGINT NOT NULL,
-  `team_name` TEXT NULL DEFAULT NULL,
-  `team_code` TEXT NULL DEFAULT NULL,
-  PRIMARY KEY (`team_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
--- Table `fifa_worldcup_2022`.`matches`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `fifa_worldcup_2022`.`matches` (
-  `match_date` DATETIME NULL DEFAULT NULL,
-  `match_time` BIGINT NULL DEFAULT NULL,
-  `match_number` BIGINT NULL DEFAULT NULL,
-  `competition_name` TEXT NULL DEFAULT NULL,
-  `city` TEXT NULL DEFAULT NULL,
-  `stadium` TEXT NULL DEFAULT NULL,
-  `team_1` TEXT NULL DEFAULT NULL,
-  `team_2` TEXT NULL DEFAULT NULL,
-  `group` TEXT NULL DEFAULT NULL,
-  `attendance` DOUBLE NULL DEFAULT NULL,
-  `match_id` BIGINT NOT NULL,
-  `competition_id` BIGINT NULL DEFAULT NULL,
-  PRIMARY KEY (`match_id`))
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
 -- Table `fifa_worldcup_2022`.`events`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `fifa_worldcup_2022`.`events` (
@@ -116,25 +81,143 @@ CREATE TABLE IF NOT EXISTS `fifa_worldcup_2022`.`events` (
   `y_location_start_mirrored` DOUBLE NULL DEFAULT NULL,
   `y_location_end` DOUBLE NULL DEFAULT NULL,
   `y_location_end_mirrored` DOUBLE NULL DEFAULT NULL,
-  `x_abs` DOUBLE NULL DEFAULT NULL,
-  `y_abs` DOUBLE NULL DEFAULT NULL,
-  `x_location_start_abs` DOUBLE NULL DEFAULT NULL,
-  `y_location_start_abs` DOUBLE NULL DEFAULT NULL,
-  `x_location_end_abs` DOUBLE NULL DEFAULT NULL,
-  `y_location_end_abs` DOUBLE NULL DEFAULT NULL,
-  `match_time_in_min` BIGINT NULL DEFAULT NULL,
-  INDEX `fk_events_teams1_idx` (`team_id` ASC) VISIBLE,
-  INDEX `fk_events_matches1_idx` (`match_id` ASC) VISIBLE,
-  CONSTRAINT `fk_events_teams1`
-    FOREIGN KEY (`team_id`)
-    REFERENCES `fifa_worldcup_2022`.`teams` (`team_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_events_matches1`
-    FOREIGN KEY (`match_id`)
-    REFERENCES `fifa_worldcup_2022`.`matches` (`match_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  `match_time_in_min` BIGINT NULL DEFAULT NULL)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `fifa_worldcup_2022`.`matches`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `fifa_worldcup_2022`.`matches` (
+  `match_date` DATETIME NULL DEFAULT NULL,
+  `match_time` BIGINT NULL DEFAULT NULL,
+  `match_number` BIGINT NULL DEFAULT NULL,
+  `competition_name` TEXT NULL DEFAULT NULL,
+  `city` TEXT NULL DEFAULT NULL,
+  `stadium` TEXT NULL DEFAULT NULL,
+  `team_1` TEXT NULL DEFAULT NULL,
+  `team_2` TEXT NULL DEFAULT NULL,
+  `group` TEXT NULL DEFAULT NULL,
+  `attendance` DOUBLE NULL DEFAULT NULL,
+  `match_id` BIGINT NULL DEFAULT NULL,
+  `competition_id` BIGINT NULL DEFAULT NULL)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `fifa_worldcup_2022`.`metricas`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `fifa_worldcup_2022`.`metricas` (
+  `match_id` BIGINT NULL DEFAULT NULL,
+  `team` TEXT NULL DEFAULT NULL,
+  `corners` BIGINT NULL DEFAULT NULL,
+  `faltas_lanzadas` BIGINT NULL DEFAULT NULL,
+  `faltas_directas_lanzadas` BIGINT NULL DEFAULT NULL,
+  `faltas_realizadas` BIGINT NULL DEFAULT NULL,
+  `penaltis` BIGINT NULL DEFAULT NULL,
+  `penaltis_marcados` BIGINT NULL DEFAULT NULL,
+  `%_acierto_penaltis` TEXT NULL DEFAULT NULL,
+  `saques_banda` BIGINT NULL DEFAULT NULL,
+  `saques_banda_recepcionados` BIGINT NULL DEFAULT NULL,
+  `%_acierto_sb` DOUBLE NULL DEFAULT NULL,
+  `ofrecimientos` BIGINT NULL DEFAULT NULL,
+  `ofrecimientos_con_movimiento` BIGINT NULL DEFAULT NULL,
+  `ofrecimientos_con_movimiento_camp_contr` BIGINT NULL DEFAULT NULL,
+  `ofrecimientos_con_movimiento_ult_terc` BIGINT NULL DEFAULT NULL,
+  `remates_totales` BIGINT NULL DEFAULT NULL,
+  `remates_totales_fa` BIGINT NULL DEFAULT NULL,
+  `remates_on_target` BIGINT NULL DEFAULT NULL,
+  `remates_on_target_fa` BIGINT NULL DEFAULT NULL,
+  `remates_off_target` BIGINT NULL DEFAULT NULL,
+  `%_remates_on_target` DOUBLE NULL DEFAULT NULL,
+  `goles` BIGINT NULL DEFAULT NULL,
+  `goles_fa` BIGINT NULL DEFAULT NULL,
+  `%_gol/on_target` DOUBLE NULL DEFAULT NULL,
+  `goles_concedidos` BIGINT NULL DEFAULT NULL,
+  `centros_totales` BIGINT NULL DEFAULT NULL,
+  `centros_completados` BIGINT NULL DEFAULT NULL,
+  `asistencias_centro` BIGINT NULL DEFAULT NULL,
+  `pases` BIGINT NULL DEFAULT NULL,
+  `pases_completados` BIGINT NULL DEFAULT NULL,
+  `%_acierto_pases` DOUBLE NULL DEFAULT NULL,
+  `pases_completados_linebreak` BIGINT NULL DEFAULT NULL,
+  `asistencias_pase` BIGINT NULL DEFAULT NULL,
+  `cambios_de_juego` BIGINT NULL DEFAULT NULL,
+  `pases_ultimo_tercio` BIGINT NULL DEFAULT NULL,
+  `pases_completados_ultimo_tercio` BIGINT NULL DEFAULT NULL,
+  `progresiones` BIGINT NULL DEFAULT NULL,
+  `intentos_regate` BIGINT NULL DEFAULT NULL,
+  `intentos_regate_ult_terc` BIGINT NULL DEFAULT NULL,
+  `regates_completados` BIGINT NULL DEFAULT NULL,
+  `regates_completados_ult_terc` BIGINT NULL DEFAULT NULL,
+  `recepciones` BIGINT NULL DEFAULT NULL,
+  `recep_behind` BIGINT NULL DEFAULT NULL,
+  `recep_3_4` BIGINT NULL DEFAULT NULL,
+  `recep_2_3` BIGINT NULL DEFAULT NULL,
+  `recep_1_2` BIGINT NULL DEFAULT NULL,
+  `recep_inside` BIGINT NULL DEFAULT NULL,
+  `%_recep_inside` DOUBLE NULL DEFAULT NULL,
+  `recep_outside` BIGINT NULL DEFAULT NULL,
+  `%_recep_outside` DOUBLE NULL DEFAULT NULL,
+  `recep_ultimo_tercio` BIGINT NULL DEFAULT NULL,
+  `bloqueos` BIGINT NULL DEFAULT NULL,
+  `despejes` BIGINT NULL DEFAULT NULL,
+  `intercepciones` BIGINT NULL DEFAULT NULL,
+  `entradas_totales` BIGINT NULL DEFAULT NULL,
+  `entradas_exitosas` BIGINT NULL DEFAULT NULL,
+  `%_entradas_exitosas` DOUBLE NULL DEFAULT NULL,
+  `take_on_against` BIGINT NULL DEFAULT NULL,
+  `pushing-on` BIGINT NULL DEFAULT NULL,
+  `presiones` BIGINT NULL DEFAULT NULL,
+  `presiones_directas` BIGINT NULL DEFAULT NULL,
+  `%_presion_directa` DOUBLE NULL DEFAULT NULL,
+  `gk_active_eng` BIGINT NULL DEFAULT NULL,
+  `gk_aerial_control` BIGINT NULL DEFAULT NULL,
+  `gk_def_support` BIGINT NULL DEFAULT NULL,
+  `gk_goalprevention_tot` BIGINT NULL DEFAULT NULL,
+  `gk_goalprevention_tot_saved` BIGINT NULL DEFAULT NULL,
+  `duelos_totales` BIGINT NULL DEFAULT NULL,
+  `duelos_totales_ganados` BIGINT NULL DEFAULT NULL,
+  `%_duelos_totales_ganados` BIGINT NULL DEFAULT NULL,
+  `duelos_campo_ganados` BIGINT NULL DEFAULT NULL,
+  `duelos_físicos_ganados` BIGINT NULL DEFAULT NULL,
+  `duelos_aereos_ganados` BIGINT NULL DEFAULT NULL,
+  `sustituciones` BIGINT NULL DEFAULT NULL,
+  `posesion` DOUBLE NULL DEFAULT NULL,
+  `posesión_disputa` DOUBLE NULL DEFAULT NULL,
+  `pases_verticales` BIGINT NULL DEFAULT NULL,
+  `pases_adelante` BIGINT NULL DEFAULT NULL,
+  `pases_atras` BIGINT NULL DEFAULT NULL,
+  `pases_horizontales` BIGINT NULL DEFAULT NULL,
+  `%_pases_verticales` BIGINT NULL DEFAULT NULL,
+  `%_pases_adelante` BIGINT NULL DEFAULT NULL,
+  `%_pases_atras` BIGINT NULL DEFAULT NULL,
+  `%_pases_horizontales` BIGINT NULL DEFAULT NULL,
+  `recuperaciones` BIGINT NULL DEFAULT NULL,
+  `recuperaciones_campo_contr` BIGINT NULL DEFAULT NULL,
+  `%_recuperaciones_campo_contr` BIGINT NULL DEFAULT NULL,
+  `recuperaciones_campo_prop` BIGINT NULL DEFAULT NULL,
+  `%_recuperaciones_campo_prop` BIGINT NULL DEFAULT NULL,
+  `recuperaciones1` BIGINT NULL DEFAULT NULL,
+  `recuperaciones_campo_contr1` BIGINT NULL DEFAULT NULL,
+  `%_recuperaciones_campo_contr1` BIGINT NULL DEFAULT NULL,
+  `recuperaciones_campo_prop1` BIGINT NULL DEFAULT NULL,
+  `%_recuperaciones_campo_prop1` BIGINT NULL DEFAULT NULL,
+  `perdidas` BIGINT NULL DEFAULT NULL,
+  `perdidas_campo_prop` BIGINT NULL DEFAULT NULL,
+  `%_perdidas_campo_prop` BIGINT NULL DEFAULT NULL,
+  `tarj_amarillas` BIGINT NULL DEFAULT NULL,
+  `tarjetas_rojas` BIGINT NULL DEFAULT NULL,
+  `asistencias_totales` BIGINT NULL DEFAULT NULL,
+  `xG` DOUBLE NULL DEFAULT NULL,
+  `distancia_recorrida_km` DOUBLE NULL DEFAULT NULL,
+  `n_speed_runs_(15-20km/h)` BIGINT NULL DEFAULT NULL,
+  `n_of_sprints_(>20km/h)` BIGINT NULL DEFAULT NULL,
+  `total_average_speed_(km/h)` DOUBLE NULL DEFAULT NULL)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
@@ -155,13 +238,19 @@ CREATE TABLE IF NOT EXISTS `fifa_worldcup_2022`.`players` (
   `player_last_name` TEXT NULL DEFAULT NULL,
   `player_date_birth` DATETIME NULL DEFAULT NULL,
   `team_code` TEXT NULL DEFAULT NULL,
-  PRIMARY KEY (`player_id`),
-  INDEX `fk_players_teams_idx` (`team_id` ASC) VISIBLE,
-  CONSTRAINT `fk_players_teams`
-    FOREIGN KEY (`team_id`)
-    REFERENCES `fifa_worldcup_2022`.`teams` (`team_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`player_id`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `fifa_worldcup_2022`.`teams`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `fifa_worldcup_2022`.`teams` (
+  `team_id` BIGINT NULL DEFAULT NULL,
+  `team_name` TEXT NULL DEFAULT NULL,
+  `team_code` TEXT NULL DEFAULT NULL)
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_0900_ai_ci;
